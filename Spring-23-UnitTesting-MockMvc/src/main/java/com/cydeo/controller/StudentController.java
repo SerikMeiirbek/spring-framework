9ab2e1e13b62a991276e07d86c3325adf5813f26
serikmeiirbek <serik.meiirbek@gmail.com>
2022-07-02 11:38:59 -0400

    Updated Student Controller

--- Spring-23-UnitTesting-MockMvc/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-23-UnitTesting-MockMvc/src/main/java/com/cydeo/controller/StudentController.java
@@ -10,20 +10,20 @@ import java.util.List;
 @RestController
 public class StudentController {
 
-    private final StudentService studentService;
-
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
+//    private final StudentService studentService;
+//
+//    public StudentController(StudentService studentService) {
+//        this.studentService = studentService;
+//    }
 
     @GetMapping("/student")
     public Student getStudent() {
         return new Student("Mike", "Smith", 20);
     }
 
-    @GetMapping("/service/student")
-    public List<Student> getStudent_Service() {
-        return studentService.getStudent();
-    }
+//    @GetMapping("/service/student")
+//    public List<Student> getStudent_Service() {
+//        return studentService.getStudent();
+//    }
 
 }
